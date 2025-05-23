fa7e6bc4045bd01fc8c2117875308b242f7fe6a2
jhickmanit <jhickman.it@gmail.com>
2025-05-23 08:06:38 -0700

    added CORS for the api path to allow ANY origin

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       allowedOrigins:
-        process.env.NODE_ENV === 'production'
-          ? [process.env.NEXT_PUBLIC_APP_FQDN || '']
-          : ['*'],
-      bodySizeLimit: '2mb',
+        process.env.NODE_ENV === "production"
+          ? [process.env.NEXT_PUBLIC_APP_FQDN || ""]
+          : ["*"],
+      bodySizeLimit: "2mb",
     },
   },
   typescript: {
@@ -17,41 +17,63 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: false,
   },
   allowedDevOrigins:
-    process.env.NODE_ENV === 'production'
-      ? [process.env.NEXT_PUBLIC_APP_FQDN || '']
-      : ['localhost'],
+    process.env.NODE_ENV === "production"
+      ? [process.env.NEXT_PUBLIC_APP_FQDN || ""]
+      : ["localhost"],
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         headers: [
           {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on',
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
           },
           {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
           },
           {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
           },
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
           },
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
+            key: "X-Content-Type-Options",
+            value: "nosniff",
           },
           {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
           },
           {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
